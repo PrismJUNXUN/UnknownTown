7ed95978f9e449c71cb8914b962d4e967f3e0411
PrismJUNXUN <362212966@qq.com>
2020-05-30 22:18:40 +0800

    后台管理页面

---
+++ src/views/management/Admin.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <navigation-bar-ele></navigation-bar-ele>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import NavigationBarEle from "../../components/back_end/NavigationBarEle";
+
+  export default {
+    name: "Admin",
+    components: {
+      NavigationBarEle
+    },
+    // 登录验证
+    created() {
+      if (!window.sessionStorage.getItem('isLogin')){
+        this.$router.push('/login')
+      }
+    }
+  }
+
+</script>
+
+<style scoped>
+@import "../../assets/css/normalize.css";
+</style>
