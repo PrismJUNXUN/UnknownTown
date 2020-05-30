18dc88782ac9b5c8ff79f9b10e262108a53c587a
PrismJUNXUN <362212966@qq.com>
2020-05-30 22:23:43 +0800

    后台管理

---
+++ src/views/management/managementchild/TypeManage.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="main">
+    <div class="main-page">
+      <page-bar>
+        <router-link slot="first" to="/admin/type/list" class="choice choice-first">列表</router-link>
+        <router-link slot="second" to="/admin/type/add" class="choice">新增</router-link>
+      </page-bar>
+      <keep-alive include="TypeAdd">
+      <router-view></router-view>
+      </keep-alive>
+    </div>
+  </div>
+</template>
+
+<script>
+  import PageBar from "../../../components/back_end/PageBar";
+  export default {
+    name: "TypeManage",
+    components: {PageBar}
+  }
+
+</script>
+
+<style scoped>
+  .main{
+    margin-top: 64px;
+    width: 100%;
+  }
+
+  .main-page{
+    width: 1140px;
+    min-width: 1140px;
+    margin: 0 auto;
+  }
+</style>
