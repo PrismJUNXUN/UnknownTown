85ea9bde754ae042735722373b0390dd2dcb56d6
PrismJUNXUN <362212966@qq.com>
2020-05-30 22:01:30 +0800

    首页

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,29 @@
 <template>
-  
+  <div id="mainPage">
+    <banner></banner>
+    <home-content></home-content>
+  </div>
+
 </template>
 
 <script>
+  import NavigationBar from "../../components/front_end/NavigationBar";
+  import Banner from "./home_component/Banner";
+  import HomeContent from "./home_component/HomeContent";
+
   export default {
-    name: "Home"
+    name: "Home",
+    components: {HomeContent, Banner, NavigationBar},
+    mounted() {
+      // const mainPage = document.getElementById('mainPage');
+      // mainPage.style.height = (document.getElementsByClassName('displayPage')[0].offsetHeight + document.getElementsByClassName('main-content')[0].offsetHeight)+"px";
+    }
   }
 </script>
 
 <style scoped>
-
+  @import "../../assets/css/normalize.css";
+  #mainPage{
+    overflow: hidden;
+  }
 </style>
